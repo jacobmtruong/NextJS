8b011c2262663f746b48dfa99f4b8ad79cec5763
Jacob Truong <jacob.truong@ymail.com>
2022-12-07 12:15:10 -0800

    object

--- nextjs-blog/pages/api/hello.js
+++ nextjs-blog/pages/api/hello.js
@@ -1,5 +1,18 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+const Users = [
+  {
+    id: 1,
+    name: 'John',
+    age: 22
+  },
+  {
+    id: 2,
+    name: 'Mary',
+    age: 24
+  }
+]
+
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json({ Users })
 }
