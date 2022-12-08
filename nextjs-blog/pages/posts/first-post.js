0193cc8c0081ddfc5e3000e2ab2d489fee6fcc13
Jacob Truong <jacob.truong@ymail.com>
2022-12-08 08:42:53 -0800

    api

--- nextjs-blog/pages/posts/first-post.js
+++ nextjs-blog/pages/posts/first-post.js
@@ -1,3 +1,35 @@
+import Link from 'next/link';
+import { useState } from 'react';
+
 export default function FirstPost() {
-    return <h1>First Post</h1>;
+
+    const [names, setNames] = useState([])
+
+    const fetchNames = async() => {
+        const res = await fetch('/api/hello')
+        const data = await res.json()
+        setNames(data)
+    }
+
+
+
+    return (
+        <>
+            <h1>First Post</h1>;
+
+            <Link href='/'>Back to Home</Link>
+
+            <button onClick={fetchNames}>Load People Names</button>
+
+            {
+                names.map((name, i) => {
+                    return (
+                            <ul key={i}>
+                                <li>{name.name}</li>
+                            </ul>
+                    )
+                })
+            }
+        </>
+    )
 }
