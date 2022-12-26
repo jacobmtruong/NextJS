727e8210a6f274e4bc19ad97cb96e365e5a21529
Jacob Truong <jacob.truong@ymail.com>
2022-12-26 15:01:40 -0800

    dynamic routes

--- first-next-js/pages/clients/index.js
+++ first-next-js/pages/clients/index.js
@@ -1,7 +1,28 @@
+import Link from "next/link";
+
 export default function ClientsPage() {
+  const clients = [
+    {
+      id: "jake",
+      name: "Jake",
+    },
+    {
+      id: "minh",
+      name: "Minh",
+    },
+  ];
+
   return (
     <div>
       <h1>Client Page</h1>
+
+      <ul>
+        {clients.map((client) => (
+          <li key={client.id}>
+            <Link href={`/clients/${client.id}`}>{client.name}</Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
