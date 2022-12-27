094dc9dd2ea0ac7b451fb4c339d9ce0d7d7ae8c9
Jacob Truong <jacob.truong@ymail.com>
2022-12-27 13:26:08 -0800

    button url

--- first-next-js/pages/clients/[id]/index.js
+++ first-next-js/pages/clients/[id]/index.js
@@ -3,9 +3,21 @@ import { useRouter } from "next/router";
 export default function GivenClientPage() {
   const router = useRouter();
 
+  const loadProject = () => {
+    router.push({
+      pathname: "/clients/[id]/[clientprojectid]",
+      query: {
+        id: "andre",
+        clientprojectid: "project a",
+      },
+    });
+  };
+
   return (
     <div>
       <h1>Specific Given Client Page</h1>
+
+      <button onClick={loadProject}>Load Project</button>
     </div>
   );
 }
