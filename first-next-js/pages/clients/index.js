610d03359ebc7db8eab3dce7f4613f29e3cba805
Jacob Truong <jacob.truong@ymail.com>
2022-12-27 13:18:16 -0800

    link

--- first-next-js/pages/clients/index.js
+++ first-next-js/pages/clients/index.js
@@ -15,11 +15,29 @@ export default function ClientsPage() {
   return (
     <div>
       <h1>Client Page</h1>
+      {/* there are 2 ways of using LINK */}
 
+      {/* 1 */}
       <ul>
         {clients.map((client) => (
           <li key={client.id}>
             <Link href={`/clients/${client.id}`}>{client.name}</Link>
+          </li>
+        ))}
+      </ul>
+
+      {/* 2 */}
+      <ul>
+        {clients.map((client) => (
+          <li key={client.id}>
+            <Link
+              href={{
+                pathname: "/clients/[id]",
+                query: { id: client.id },
+              }}
+            >
+              {client.name}
+            </Link>
           </li>
         ))}
       </ul>
