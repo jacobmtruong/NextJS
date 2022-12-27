375ba051cc73f29d164950eafdb6d1659b3d541a
Jacob Truong <jacob.truong@ymail.com>
2022-12-27 13:18:07 -0800

    link

--- first-next-js/pages/clients/[id]/index.js
+++ first-next-js/pages/clients/[id]/index.js
@@ -3,8 +3,6 @@ import { useRouter } from "next/router";
 export default function GivenClientPage() {
   const router = useRouter();
 
-  console.log(router.query);
-
   return (
     <div>
       <h1>Specific Given Client Page</h1>
