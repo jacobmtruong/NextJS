c1653ae179fa580d1274d2c729b01a2a7b8f7926
Jacob Truong <jacob.truong@ymail.com>
2022-12-30 09:40:13 -0800

    icons

--- event-planner-project/components/events/event-item.js
+++ event-planner-project/components/events/event-item.js
@@ -1,5 +1,8 @@
 import classes from "./event-item.module.css";
 import Button from "../ui/button";
+import AddressIcon from "../icons/address-icon";
+import ArrowRightIcon from "../icons/arrow-right-icon";
+import DateIcon from "../icons/date-icon";
 
 export default function EventItem(props) {
   const { title, image, location, date, id } = props;
@@ -21,14 +24,21 @@ export default function EventItem(props) {
         <div className={classes.summary}>
           <h2>{title}</h2>
           <div className={classes.date}>
+            <DateIcon />
             <time>{readableDate}</time>
           </div>
-          <div>
-            <address className={classes.address}>{formattedAddress}</address>
+          <div className={classes.address}>
+            <AddressIcon />
+            <address>{formattedAddress}</address>
           </div>
         </div>
         <div className={classes.actions}>
-          <Button link={exploreLink}>Explore Event</Button>
+          <Button link={exploreLink}>
+            <span>Explore Event</span>
+            <span className={classes.icon}>
+              <ArrowRightIcon />
+            </span>
+          </Button>
         </div>
       </div>
     </li>
