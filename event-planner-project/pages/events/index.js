85dcd9b0db5717ca0aa8668dd3e717f7e9371ae7
Jacob Truong <jacob.truong@ymail.com>
2023-01-12 16:58:05 +0700

    commit

--- event-planner-project/pages/events/index.js
+++ event-planner-project/pages/events/index.js
@@ -1,14 +1,21 @@
 import { Fragment } from "react";
 import { getAllEvents } from "../../events";
+import { useRouter } from "next/router";
 import EventList from "../../components/events/event-list";
 import EventSearch from "../../components/events/event-search";
 
 function AllEventsPage() {
   const events = getAllEvents();
+  const router = useRouter();
+
+  function findEventHandler(year, month) {
+    const fullPath = `/events/${year}/${month}`;
+    router.push(fullPath);
+  }
 
   return (
     <Fragment>
-      <EventSearch />
+      <EventSearch onSearch={findEventHandler} />
       <EventList items={events} />
     </Fragment>
   );
