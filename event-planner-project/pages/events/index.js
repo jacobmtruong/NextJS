cb329018fff7d16e55fa42fe54eea9d770cfa3e3
Jacob Truong <jacob.truong@ymail.com>
2023-01-12 15:15:06 +0700

    all events page

--- event-planner-project/pages/events/index.js
+++ event-planner-project/pages/events/index.js
@@ -1,36 +1,14 @@
-import { getFeaturedEvents } from "../../events";
+import { Fragment } from "react";
+import EventList from "../../components/events/event-list";
+import { getAllEvents } from "../../events";
 
-export default function AllEvents() {
-  const events = getFeaturedEvents();
+function AllEventsPage() {
+  const events = getAllEvents();
+
   return (
-    <>
-      <h1>All Events Page</h1>
-      <div>
-        <ul>
-          {events.map((e) => (
-            <li key={e.id}>
-              <div
-                style={{
-                  border: "2px solid black",
-                  width: "fit-content",
-                  padding: "20px",
-                }}
-              >
-                <img
-                  src={e.image}
-                  alt="event photo"
-                  style={{ width: "300px" }}
-                />
-
-                <h3>{e.title}</h3>
-                <p>{e.date}</p>
-                <p>{e.location}</p>
-                <button>Explore Event</button>
-              </div>
-            </li>
-          ))}
-        </ul>
-      </div>
-    </>
+    <Fragment>
+      <EventList items={events} />
+    </Fragment>
   );
 }
+export default AllEventsPage;
